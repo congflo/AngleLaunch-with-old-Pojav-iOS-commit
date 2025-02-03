a41b7d467174d0a65aca98c091e4c605ddccc0fc
congflo <chucong491@gmail.com>
2025-02-03 22:01:18 +0700

    Remove some codes

--- src/main/java/net/congcq/anglelaunch/AngleLauncher.java
+++ src/main/java/net/congcq/anglelaunch/AngleLauncher.java
@@ -69,9 +69,6 @@ public class AngleLauncher extends UIApplicationDelegateAdapter {
         System.setProperty("prism.verbose", "true");
         System.setProperty("glass.platform", "ios");
 
-        System.setProperty("org.lwjgl.util.Debug", "true");
-        System.setProperty("org.lwjgl.util.DebugLoader", "true");
-
         System.out.println("Starting UI...");
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(args, null, AngleLauncher.class);
